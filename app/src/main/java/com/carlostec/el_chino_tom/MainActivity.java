babdc943f046f9e0f3121bdda5d41561ca824504
Carlos_Alberto_Sanchez <95440504+CallmeCarlos503@users.noreply.github.com>
2023-03-18 21:55:31 -0600

    Avance

---
+++ app/src/main/java/com/carlostec/el_chino_tom/MainActivity.java
@@ -0,0 +1,26 @@
+package com.carlostec.el_chino_tom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView Registro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Registro=findViewById(R.id.register_text);
+        Registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, registro.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
